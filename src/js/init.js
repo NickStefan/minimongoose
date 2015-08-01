147d4935bfa5916db3ede2f87bb8a99be40f8cf8
Nick Stefan <NickStefan12@gmail.com>
2015-08-01 11:18:25 -0700

    basic minimongo on client

--- src/js/init.js
+++ src/js/init.js
@@ -1,1 +1,23 @@
-alert('hello world');
+var minimongo = require('minimongo');
+
+var LocalDb = minimongo.MemoryDb;
+
+var db = new LocalDb();
+
+db.addCollection('Brand');
+db.addCollection('Car');
+
+db.Brand.upsert({
+    name: 'BMW',
+    updated_at: new Date()
+}, function(brand){
+
+    db.Car.upsert({
+        name: '325i',
+        brand: brand._id,
+        brand_id: brand._id,
+        updated_at: new Date()
+    }, function (car){
+        console.log(car);
+    });
+});
